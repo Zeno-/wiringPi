783577856701c29af8b39747d70e67c2f2361198
Craig Robbins <kde.psych@gmail.com>
2016-09-30 19:40:00 +1000

    Revert "Use write(2) for wiringPiSPIDataWrite() instead of ioctl()"
    This reverts commit b883085de87a2be8b933a9095d7b55bb0238fb91.

--- wiringPi/wiringPiSPI.c
+++ wiringPi/wiringPiSPI.c
@@ -28,7 +28,6 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <unistd.h>
 #include <linux/spi/spidev.h>
 
 #include "wiringPi.h"
@@ -96,11 +95,25 @@ int wiringPiSPIDataRW (int channel, unsigned char *data, int len)
  *********************************************************************************
  */
 
-ssize_t wiringPiSPIDataWrite(int channel, const unsigned char *data, int len)
+int wiringPiSPIDataWrite(int channel, const unsigned char *data, int len)
 {
+  struct spi_ioc_transfer spi ;
+
   channel &= 1 ;
 
-  return write(spiFds[channel], data, len);
+// Mentioned in spidev.h but not used in the original kernel documentation
+//	test program )-:
+
+  memset (&spi, 0, sizeof (spi)) ;
+
+  spi.tx_buf        = (unsigned long)data ;
+  spi.rx_buf        = 0;
+  spi.len           = len ;
+  spi.delay_usecs   = spiDelay ;
+  spi.speed_hz      = spiSpeeds [channel] ;
+  spi.bits_per_word = spiBPW ;
+
+  return ioctl (spiFds [channel], SPI_IOC_MESSAGE(1), &spi) ;
 }
 
 
